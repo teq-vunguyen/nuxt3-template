23c7157dcfd0d0645cf214ab012e1677b98ef54f
Vu Nguyen <vunguyen@teqnological.asia>
2023-06-05 09:56:50 +0700

    initial commit

---
+++ components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button :class="[props.type]">{{ props.text }}</button>
+</template>
+<script setup lang="ts">
+interface Props {
+  text: string;
+  type: "primary" | "secondary" | "tertiary";
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  text: "Vu Nguyen",
+});
+
+console.log(props.text);
+console.log(props.type);
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>
+      <Button type="secondary" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Button from "~/components/button.vue";
+console.log("hello");
+</script>
